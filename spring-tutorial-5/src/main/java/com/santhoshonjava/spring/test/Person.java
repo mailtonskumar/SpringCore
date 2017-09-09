c7c5cd0af418dda6e286441703a853fba445d3ba
Santhosh Kumar Nunna <mailtonskumar@gmail.com>
2017-09-09 12:36:26 +0530

    Sample Project

---
+++ spring-tutorial-5/src/main/java/com/santhoshonjava/spring/test/Person.java
@@ -0,0 +1,16 @@
+package com.santhoshonjava.spring.test;
+
+/**
+ * 
+ * @author SanthoshKumarN
+ *
+ */
+public class Person {
+	
+	/**
+	 * 
+	 */
+	public void speak() {
+		System.out.println("Hello! I'm a person.");
+	}
+}
